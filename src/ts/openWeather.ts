a452caa27e1ac2feed48a63ac7ad403c8e248af9
Christian Predoianu <christianpredoianu@yahoo.com>
2022-08-25 16:26:17 +0200

    overloaded fetch function + refactoring

--- src/ts/openWeather.ts
+++ src/ts/openWeather.ts
@@ -1,15 +1,20 @@
 import { Position } from '../types/position.interface';
 
-export async function getWeatherData(position: Position) {
+export async function getWeatherData(position: string): Promise<any>;
+export async function getWeatherData(position: Position): Promise<any>;
+
+export async function getWeatherData(position: any) {
   const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?';
+  const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+  let locationUrl;
+
+  typeof position === 'string'
+    ? (locationUrl = `${baseUrl}q=${position}&appid=${apiKey}&units=metric`)
+    : (locationUrl = `${baseUrl}lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric
+  `);
 
   try {
-    const response = await fetch(
-      `${baseUrl}lat=${position.coords.latitude}&lon=${
-        position.coords.longitude
-      }&appid=${import.meta.env.VITE_OPENWEATHER_API_KEY}&units=metric
-      `
-    );
+    const response = await fetch(locationUrl);
     const data = await response.json();
     return data;
   } catch (error) {
