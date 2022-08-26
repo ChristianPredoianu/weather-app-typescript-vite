ea79e1b1fc1e6ab1925b07d5f3652fb0bde1c91f
Christian Predoianu <christianpredoianu@yahoo.com>
2022-08-26 14:34:21 +0200

    adde if tl.isActive()

--- src/ts/gsap.ts
+++ src/ts/gsap.ts
@@ -1,10 +1,13 @@
 import gsap from 'gsap';
 
+const tl = gsap.timeline({ defaults: { duration: 0.4 } });
+
 export function animateApp() {
-  const tl = gsap.timeline({ duration: 0.1 });
-  tl.from('#location', { x: 100, opacity: 0 });
-  tl.from('#weather-icon', { opacity: 0 });
-  tl.from('#degrees-div', { opacity: 0 });
-  tl.from('#additional-weather-info', { y: 100, opacity: 0 });
-  tl.from('.info-card', { y: 100, stagger: 0.5, opacity: 0, rotate: 10 });
+  if (!tl.isActive()) {
+    tl.from('#location', { x: 100, opacity: 0 });
+    tl.from('#weather-icon', { opacity: 0 });
+    tl.from('#degrees-div', { opacity: 0 });
+    tl.from('#additional-weather-info', { y: 100, opacity: 0 });
+    tl.from('.info-card', { y: 100, stagger: 0.1, opacity: 0, rotate: 10 });
+  }
 }
